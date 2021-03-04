02bf926bb1ac7e0c49bb39f3eb8612723e1a9e82
Oro <Oroshanh@hotmail.com>
2021-03-04 20:44:34 +0100

    Initial commit

---
+++ src/test/java/webprog/oblig2/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package webprog.oblig2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
